acc99c014ef2f9ec135b5a49794c837500ad6d8a
wersder <wersder.dev@gmail.com>
2026-06-12 20:01:30 +0200

    feat: resolve dependency issues

--- src/main/java/com/reefbot/service/game/handlers/TideGameHandler.java
+++ src/main/java/com/reefbot/service/game/handlers/TideGameHandler.java
@@ -16,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendDice;
-import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
